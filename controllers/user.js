df84261f410bee5c77747971f18ab17085bb6a0f
hrd05 <harshdunkhwal55@gmail.com>
2023-10-19 15:28:40 +0530

    password encrypted

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 
 const path = require('path');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/userSignup');
 const { where } = require('sequelize');
@@ -14,39 +15,41 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    User.create({
-        name,
-        email,
-        password
+    bcrypt.hash(password, 10, (err, hash) => {
+        console.log(err);
+        User.create({ name, email, password: hash })
+            .then(() => {
+                res.sendFile(path.join(__dirname, '../', 'views', 'login.html'));
+
+            })
+            .catch(err => console.log(err));
     })
-    .then(() => {
-        res.sendFile(path.join(__dirname,'../', 'views', 'login.html' ));
-    })
-    .catch(err => console.log(err));
 
 };
 
 exports.getLogin = (req, res, next) => {
-    res.sendFile(path.join(__dirname,'../', 'views', 'login.html' ));
+    res.sendFile(path.join(__dirname, '../', 'views', 'login.html'));
 }
 
-exports.postLogin = (req, res, next ) => {
+exports.postLogin = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    User.findOne({where: {email}})
-    .then((user) => {
-        if(!user){
-            res.status(404).json({message: 'user not found'});
-        }
-        else{
-            if(user.password === password){
-                res.status(200).json({message: 'User logged succesfully'});
+    User.findOne({ where: { email } })
+        .then((user) => {
+            if (!user) {
+                res.status(404).json({ message: 'user not found' });
             }
-            else{
-                res.status(401).json({message: 'incorrect password'});
+            else {
+                bcrypt.compare(password, user.password, (err, response) => {
+                    if (!err) {
+                        res.status(200).json({ message: 'User logged succesfully' });
+                    }
+                    else {
+                        res.status(401).json({ message: 'incorrect password' });
+                    }
+                })
             }
-        }
-    })
-    .catch(err => console.log(err));
+        })
+        .catch(err => console.log(err));
 };
