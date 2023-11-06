ed19fe9d512658d7e8c93c1d82a756fc6a0595c2
hrd05 <harshdunkhwal55@gmail.com>
2023-11-06 13:42:41 +0530

    space error

--- routes/reset.js
+++ routes/reset.js
@@ -9,7 +9,7 @@ router.get('/password/forgotpassword' , resetController.getforgotpassword);
 
 router.get('/password/updatepassword/:resetpasswordid', resetController.updatepassword);
 
-router.get('/password   /resetpassword/:id', resetController.resetpassword);
+router.get('/password/resetpassword/:id', resetController.resetpassword);
 
 router.post('/password/forgotpassword', resetController.forgotpassword);
 
